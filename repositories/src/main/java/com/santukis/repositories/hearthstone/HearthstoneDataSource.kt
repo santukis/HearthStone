ab529bc54a75d94272169c10f8a2190a1ad8b633
David <santamadavid@gmail.com>
2022-06-18 13:09:24 +0200

    created searchCards and saveCards methods on HearthstoneDataSource.kt

--- repositories/src/main/java/com/santukis/repositories/hearthstone/HearthstoneDataSource.kt
+++ repositories/src/main/java/com/santukis/repositories/hearthstone/HearthstoneDataSource.kt
@@ -1,7 +1,9 @@
 package com.santukis.repositories.hearthstone
 
+import com.santukis.entities.hearthstone.Card
 import com.santukis.entities.hearthstone.Deck
 import com.santukis.entities.hearthstone.DeckRequest
+import com.santukis.entities.hearthstone.SearchCardsRequest
 import com.santukis.repositories.strategies.defaultError
 
 interface HearthstoneDataSource {
@@ -9,4 +11,8 @@ interface HearthstoneDataSource {
     suspend fun getDeck(deckRequest: DeckRequest): Result<Deck> = defaultError()
 
     suspend fun saveDeck(deck: Deck): Result<Deck> = defaultError()
+
+    suspend fun searchCards(searchCardsRequest: SearchCardsRequest): Result<List<Card>> = defaultError()
+
+    suspend fun saveCards(cards: List<Card>): Result<List<Card>> = defaultError()
 }
